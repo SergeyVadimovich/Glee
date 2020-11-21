9d56e8c7f0d92331a93cc929438558c912494637
SergeyVadimovich <SadichenkoSV@gmail.com>
2020-11-21 14:13:01 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-  
+  $('.slider__inner').slick({
+    arrows:false,
+    dots:true,
+    fade:true,
+    autoplay:true,
+    autoplaySpeed:2000,
+    });
+
+    var mixer = mixitup('.product__items');
 });
