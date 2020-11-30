dbc1c90bd721e6face03226b41a7f1d352e5433f
SergeyVadimovich <SadichenkoSV@gmail.com>
2020-11-30 21:19:24 +0200

    added filters added pagination added sort

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ function styles() {
   .pipe(concat('style.min.css'))
   .pipe(autoprefixer({
     overrideBrowserslist:['last 10 versions'],
-    grid: true
+    grid: true,
   }))
   .pipe(dest('app/css'))
   .pipe(browserSync.stream())
@@ -50,6 +50,8 @@ function scripts(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/deadline.js',
     'app/js/main.js'
   ])
